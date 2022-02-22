c606f02402d408be372467195111d6c65af163f5
Artur Kutusenko <kutusenko@gmail.com>
2022-02-22 23:09:51 +0200

    fix some lint bugs

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,56 +1,55 @@
-import {Injectable} from '@nestjs/common';
-import {InjectRepository} from '@nestjs/typeorm';
-import {User} from './users.entity';
-import {getConnection, Repository} from 'typeorm';
-import {CreateUserDto} from './dto/create.user.dto';
-import {oneUser} from "./dto/findOne.user.dto";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { getConnection, Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create.user.dto';
+import { oneUser } from './dto/findOne.user.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(User)
-        private usersRepository: Repository<User>,
-    ) {
-    }
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
 
-    async createUser(dto: CreateUserDto):Promise<User> {
-        try {
-            const user = await this.usersRepository.save(dto);
-            return user;
-        } catch (e) {
-            return e
-        }
+  async createUser(dto: CreateUserDto): Promise<User> {
+    try {
+      const user = await this.usersRepository.save(dto);
+      return user;
+    } catch (e) {
+      return e;
     }
+  }
 
-    async getAllUsers():Promise<User[]> {
-        try {
-            const users = await this.usersRepository.find();
-            return users;
-        } catch (e) {
-            return e
-        }
+  async getAllUsers(): Promise<User[]> {
+    try {
+      const users = await this.usersRepository.find();
+      return users;
+    } catch (e) {
+      return e;
     }
+  }
 
-    async getOneUser(id: oneUser):Promise<User> {
-        try {
-            const user = await this.usersRepository.findOne({where: id});
-            return user;
-        } catch (e) {
-            return e
-        }
+  async getOneUser(id: oneUser): Promise<User> {
+    try {
+      const user = await this.usersRepository.findOne({ where: id });
+      return user;
+    } catch (e) {
+      return e;
     }
+  }
 
-    async removeUser(id: oneUser):Promise<string> {
-        try {
-            await getConnection()
-                .createQueryBuilder()
-                .delete()
-                .from(User)
-                .where("id = :id", {id})
-                .execute();
-            return 'Успішне видалення'
-        } catch (e) {
-            return e
-        }
+  async removeUser(id: oneUser): Promise<string> {
+    try {
+      await getConnection()
+        .createQueryBuilder()
+        .delete()
+        .from(User)
+        .where('id = :id', { id })
+        .execute();
+      return 'User deletion completed successfully';
+    } catch (e) {
+      return e;
     }
+  }
 }
