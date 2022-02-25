4d62960b8e70ea247a0e5e495dc493e16a4f63eb
andrewNEW42 <sanjino56@gmail.com>
2022-02-25 14:32:33 +0200

    check email in database before sending a mail

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
+import { User } from '../users/users.entity';
 
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
   
   async sendResetPasswordMail(email:string){
-    return this.mailerService.sendMail({
-      to: `${email}`,
-      from: process.env.MAIL_DEFAULT_FROM,
-      subject: 'Welcome!',
-      text: 'Use this password',
-      html: `<div>
-              <h1>Hello ${email}!</h1>
-              <p>A request has been received to change the password for your account.</p>
-              <a href="https://www.google.com" target="_blank">
-                <button>Reset password</button>
-              </a>
-            </div>`,
-    });
+    try {
+      const user = await User.findOne({where: {
+        email: email,
+      }
+    })
+    if (user) {
+      return this.mailerService.sendMail({
+        to: `${email}`,
+        from: process.env.MAIL_DEFAULT_FROM,
+        subject: 'Welcome!',
+        text: 'Use this password',
+        html: `<div>
+                <h1>Hello ${email}!</h1>
+                <p>A request has been received to change the password for your account.</p>
+                <a href="https://www.google.com" target="_blank">
+                  <button>Reset password</button>
+                </a>
+              </div>`,
+      })
+    } else {
+      throw "Email not registered"
+    }
+    } catch (error) {
+      return error
+    }
   }
   }
