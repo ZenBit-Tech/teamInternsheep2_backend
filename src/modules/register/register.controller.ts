dda2392c40eb7c4532924b832046f59921f070be
Artem Zhurbey <zzombi1@rambler.ua>
2022-03-24 17:53:56 +0200

    add jwt to registration

--- src/modules/register/register.controller.ts
+++ src/modules/register/register.controller.ts
@@ -5,14 +5,14 @@ import { User } from '../users/users.entity';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Регистрация')
-@Controller('register')
+@Controller('auth')
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
 
   @ApiOperation({ summary: 'User creation' })
   @ApiResponse({ status: 200, type: User })
-  @Post()
-  create(@Body() userDto: CreateUserDto): Promise<User> {
-    return this.registerService.createUser(userDto);
+  @Post('/registration')
+  create(@Body() userDto: CreateUserDto) {
+    return this.registerService.registration(userDto);
   }
 }
--- src/modules/register/register.module.ts
+++ src/modules/register/register.module.ts
@@ -3,10 +3,19 @@ import { RegisterService } from './register.service';
 import { RegisterController } from './register.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/users.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [RegisterController],
   providers: [RegisterService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.SESSION_SECRET || 'secret',
+      signOptions: {
+        expiresIn: '24h',
+      },
+    }),
+  ],
 })
 export class RegisterModule {}
