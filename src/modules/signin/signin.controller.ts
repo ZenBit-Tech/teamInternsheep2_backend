a56da0867995b162d4d90c8a8fe18a963a1fe90e
Artem Zhurbey <zzombi1@rambler.ua>
2022-03-29 21:48:41 +0300

    add jwt to login

--- src/modules/signin/signin.controller.ts
+++ src/modules/signin/signin.controller.ts
@@ -1,6 +1,14 @@
 require('isomorphic-fetch');
-import { Controller, Body, Post, Get, Req, UseGuards, Patch} from '@nestjs/common';
-import { SigninFormDto } from "../dto/signin.user.dto";
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Req,
+  UseGuards,
+  Patch,
+} from '@nestjs/common';
+import { SigninFormDto } from '../dto/signin.user.dto';
 import { SigninService } from './signin.service';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -15,25 +23,30 @@ export class SigninController {
   @Get('google/redirect')
   @UseGuards(AuthGuard('google'))
   googleAuthRedirect(@Req() req) {
-    return this.SigninService.googleLogin(req)
+    return this.SigninService.googleLogin(req);
   }
 
   @Post('auth/signin')
-    authorize(@Body() formData:SigninFormDto){
-      return this.SigninService.signInByEmail(formData);
+  authorize(@Body() formData: SigninFormDto) {
+    return this.SigninService.signInByEmail(formData);
   }
 
   @Patch('update-password')
-    update(@Body() formData:{email:string, password:string}) {
-      return this.SigninService.updateUserPassword(formData);
-}
+  update(@Body() formData: { email: string; password: string }) {
+    return this.SigninService.updateUserPassword(formData);
+  }
+
   @Get('get-reset-password-session')
-    check(@Req() request){
-      request.sessionStore.all((err, sessions)=>{ console.log(sessions);
-       })
-      const {session_id} = request.body
-      if (request.session[`passwordReset${session_id}`] && request.session[`passwordReset${session_id}`].session_id) {
-        return request.session[`passwordReset${session_id}`]
-      }
+  check(@Req() request) {
+    request.sessionStore.all((err, sessions) => {
+      console.log(sessions);
+    });
+    const { session_id } = request.body;
+    if (
+      request.session[`passwordReset${session_id}`] &&
+      request.session[`passwordReset${session_id}`].session_id
+    ) {
+      return request.session[`passwordReset${session_id}`];
     }
+  }
 }
--- src/modules/signin/signin.service.ts
+++ src/modules/signin/signin.service.ts
@@ -1,84 +1,113 @@
-import {Injectable} from '@nestjs/common';
-import {SigninFormDto} from "../dto/signin.user.dto";
-import {User} from '../users/users.entity';
-import {getConnection} from 'typeorm';
-import * as bcrypt from 'bcrypt'
-import {CreateUserDto} from "../dto/create.user.dto";
+import {Injectable, UnauthorizedException} from '@nestjs/common';
+import { SigninFormDto } from '../dto/signin.user.dto';
+import { User } from '../users/users.entity';
+import {getConnection, Repository} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import {InjectRepository} from "@nestjs/typeorm";
 
 
-require('dotenv').config()
+require('dotenv').config();
 
 @Injectable()
 export class SigninService {
-    async signInByEmail(formData: SigninFormDto): Promise<User | string> {
-        try {
-            const user = await User.findOne({
-                where: {
-                    email: formData.email,
-                }
-            })
-            if (!user) {
-                throw "Incorrect email or password"
-            }
-            const isMatch = await bcrypt.compare(formData.password, user.password)
-            if (user && isMatch) {
-                delete user.password;
-                return user;
-            } else {
-                throw "Incorrect email or password"
-            }
-        } catch (error) {
-            return error
-        }
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
 
+  async signInByEmail(dto: SigninFormDto) {
+    const user = await this.validateUser(dto);
+    if (user.status === 401) return user;
+
+    return this.generateToken(user);
+  }
+
+  async validateUser(dto: SigninFormDto) {
+    try {
+      const user = await User.findOne({
+        where: {
+          email: dto.email,
+        },
+      });
+      if (!user)
+        throw new UnauthorizedException({
+          message: 'Incorrect email ',
+        });
+      const isMatch = await bcrypt.compare(dto.password, user.password);
+      if (!isMatch)
+        throw new UnauthorizedException({
+          message: 'Incorrect password ',
+        });
+      if (user && isMatch) {
+        return user;
+      }
+    } catch (error) {
+      return error;
     }
+  }
 
+  private async generateToken(user: User) {
+    const payload = {
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      id: user.id,
+      userRole: user.userRole,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
 
-    async googleLogin(req): Promise<User | string> {
-        if (!req.user) {
-            return 'No user from google';
-        } else {
-            let user = await User.findOne({
-                where: {
-                    email: req.user.email,
-                }
-            })
-            if (user) {
-                delete user.password;
-                return user;
-            } else {
-                const option:object = {
-                    firstName: req.user.firstName,
-                    lastName: req.user.lastName ? req.user.lastName : '',
-                    email: req.user.email,
-                    password: '',
-                    phoneNumber: null,
-                    userRole: null,
-                }
-                // @ts-ignore
-                user = await User.save(option)
-                delete user.password;
-                return user
-            }
-        }
+  async googleLogin(req): Promise<User | string> {
+    if (!req.user) {
+      return 'No user from google';
     }
-
-    async updateUserPassword(formData): Promise<string> {
-        const hash = await bcrypt.hash(formData.password, Number(process.env.SALT_OR_ROUNDS))
-        if (hash) {
-            try {
-                await getConnection()
-                    .createQueryBuilder()
-                    .update(User)
-                    .set({password: hash})
-                    .where({email: formData.email})
-                    .execute();
-            } catch (error) {
-                return error
-            }
-            return "Password has been updated."
-        } else {
-            return "An error occurred."
-        }
+    let user = await User.findOne({
+      where: {
+        email: req.user.email,
+      },
+    });
+    if (user) {
+      delete user.password;
+      return user;
     }
+    const option: object = {
+      firstName: req.user.firstName,
+      lastName: req.user.lastName ? req.user.lastName : '',
+      email: req.user.email,
+      password: '',
+      phoneNumber: null,
+      userRole: null,
+    };
+    // @ts-ignore
+    user = await User.save(option);
+    delete user.password;
+    return user;
+  }
+
+  async updateUserPassword(formData): Promise<string> {
+    const hash = await bcrypt.hash(
+      formData.password,
+      Number(process.env.SALT_OR_ROUNDS),
+    );
+    if (hash) {
+      try {
+        await getConnection()
+          .createQueryBuilder()
+          .update(User)
+          .set({ password: hash })
+          .where({ email: formData.email })
+          .execute();
+      } catch (error) {
+        return error;
+      }
+      return 'Password has been updated.';
+    }
+    return 'An error occurred.';
+  }
+
 }
