19526a3e8ac797f28d65f580353e962d8368f2c3
Artem Zhurbey <zzombi1@rambler.ua>
2022-04-05 20:04:58 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: String(process.env.CLIENT_URL), credentials: true });
 
   const swagger = new DocumentBuilder()
     .setTitle('team 2')
@@ -24,4 +25,5 @@ async function start() {
 
   await app.listen(PORT, () => console.log(`Server started on port:${PORT}`));
 }
+
 start();
