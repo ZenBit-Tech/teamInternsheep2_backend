16a32084e374f49f87d0f2da13760943782fde4a
ZhurbeyArtem <88031301+ZhurbeyArtem@users.noreply.github.com>
2022-04-19 02:05:36 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import * as session from 'express-session';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
@@ -14,14 +13,6 @@ async function start() {
     .build();
   const document = SwaggerModule.createDocument(app, swagger);
   SwaggerModule.setup('/api/docs', app, document);
-
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
 
   await app.listen(PORT, () => console.log(`Server started on port:${PORT}`));
 }
