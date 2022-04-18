c33181625957f3985598f167efe2de0d001dd741
Artem Zhurbey <zzombi1@rambler.ua>
2022-04-19 02:02:17 +0300

    transfer connect from user.module to this file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { GoogleStrategy } from "./modules/google/google.strategy";
-import { MailModule } from './modules/mail/mail.module';;
+import { GoogleStrategy } from './modules/google/google.strategy';
+import { MailModule } from './modules/mail/mail.module';
 import { SettingsModule } from './modules/settings/settings.module';
+import { JobModule } from './modules/job/job.module';
+import { TagsModule } from './modules/tags/tags.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [AppService],
@@ -11,7 +16,25 @@ import { SettingsModule } from './modules/settings/settings.module';
     UsersModule,
     MailModule,
     GoogleStrategy,
-    SettingsModule
+    SettingsModule,
+    JobModule,
+    TagsModule,
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
   ],
 })
 export class AppModule {}
