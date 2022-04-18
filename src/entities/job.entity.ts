e30354a9af11098e01e87ecdb26888138021c067
Artem Zhurbey <zzombi1@rambler.ua>
2022-04-19 01:51:43 +0300

    create new entities and move them to new folder

---
+++ src/entities/job.entity.ts
@@ -0,0 +1,50 @@
+import {
+  BaseEntity,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+import { TagsJob } from './tags_job.entity';
+import { Bid } from './bid.entity';
+
+@Entity()
+export class Job extends BaseEntity {
+  @ApiProperty({ example: '1', description: 'Unique identificator' })
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number;
+
+  @ApiProperty({ example: 'connect your site to cms', description: 'title' })
+  @Column({ type: 'varchar', width: 255, unique: true })
+  title: string;
+
+  @ApiProperty({ example: 'create api for bot', description: 'description' })
+  @Column({ type: 'text' })
+  description: string;
+
+  @ApiProperty({ example: '15$', description: 'Hourly rate' })
+  @Column({ type: 'varchar', width: 50 })
+  hourlyRate: string;
+
+  @ApiProperty({
+    example: '3 day',
+    description: 'time need that u finish order',
+  })
+  @Column({ type: 'varchar', width: 50 })
+  duration: string;
+
+  @ApiProperty({ example: 'advanced', description: 'lvl of english' })
+  @Column({
+    type: 'enum',
+    enum: ['Advanced', 'Intermediate', 'Elementary'],
+  })
+  englishLevel: string;
+
+  @OneToMany(() => TagsJob, (tagsToJob) => tagsToJob.job)
+  tagsToJobs!: TagsJob[];
+
+  @OneToMany(() => Bid, (bid) => bid.job)
+  bids: Bid[];
+}
