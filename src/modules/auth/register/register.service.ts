a4b5fe2778b4cf08f94ad97c156ca899d9ce8bcf
Artem Zhurbey <zzombi1@rambler.ua>
2022-04-21 01:22:27 +0300

    do little changes

--- src/modules/auth/register/register.service.ts
+++ src/modules/auth/register/register.service.ts
@@ -2,17 +2,17 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-
-import { User } from '../../../entities/users.entity';
 import { Repository } from 'typeorm';
+
+import { User } from 'src/entities/users.entity';
 import { CreateUserDto } from '../../dto/create.user.dto';
 
 @Injectable()
 export class RegisterService {
   constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-    private readonly jwtService: JwtService,
+      @InjectRepository(User)
+      private usersRepository: Repository<User>,
+      private readonly jwtService: JwtService,
   ) {}
 
   async registration(dto: CreateUserDto) {
@@ -20,20 +20,20 @@ export class RegisterService {
       let candidate = await this.getUserByEmail(dto.email);
       if (candidate) {
         throw new HttpException(
-          'a user with the same email already exists ',
-          HttpStatus.BAD_REQUEST,
+            'a user with the same email already exists ',
+            HttpStatus.BAD_REQUEST,
         );
       }
       candidate = await this.getUserByPhone(dto.phoneNumber);
       if (candidate) {
         throw new HttpException(
-          'a user with the same phone number already exists ',
-          HttpStatus.BAD_REQUEST,
+            'a user with the same phone number already exists ',
+            HttpStatus.BAD_REQUEST,
         );
       }
       const hashPassword = await bcrypt.hash(
-        dto.password,
-        Number(process.env.SALT_OR_ROUNDS),
+          dto.password,
+          Number(process.env.SALT_OR_ROUNDS),
       );
       const user = await this.usersRepository.save({
         ...dto,
